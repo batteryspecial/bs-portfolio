49d88d37f2662b4181795dc99c1bad571d126b53
batteryspecial <infinityscrub74@gmail.com>
2026-06-11 23:08:13 -0600

    config changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,5 +14,3 @@ export default {
     },
     plugins: [],
 }
-
-export const darkMode = 'class'; // Enable
--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 // vite.config.js
+import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
@@ -6,5 +7,10 @@ export default defineConfig({
   plugins: [vue()],
   build: {
     target: ['es2018', 'safari13'], // Adjust based on your Safari version
+  },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
   },
 });
