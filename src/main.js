2219a7fbb990a44e5903b05cebed61b0d56799a7
batteryspecial <infinityscrub74@gmail.com>
2026-08-28 13:45:08 -0600

    feat: enable vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,15 +1,16 @@
-import { createApp } from 'vue'
-import router from './router'
-import App from './App.vue'
-import './style.css'
-import { inject } from '@vercel/analytics'
+import { Analytics } from "@vercel/analytics/next";
+import { inject } from '@vercel/analytics';
 
+import { createApp } from 'vue';
+import router from './router';
+import App from './App.vue';
+import './style.css';
 
 inject();
 router.afterEach(() => {
     if (window.va) {
         window.va.track('pageview')
     }
-})
+});
 
-createApp(App).use(router).mount('#app')
+createApp(App).use(router).mount('#app');
