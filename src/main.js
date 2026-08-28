4e21fe828fdabf37c193a44df563b948d691b547
batteryspecial <infinityscrub74@gmail.com>
2026-08-28 14:01:55 -0600

    feat: disable vercel analytics

--- src/main.js
+++ src/main.js
@@ -1,16 +1,12 @@
-import { Analytics } from "@vercel/analytics/next";
-import { inject } from '@vercel/analytics';
-
+import { inject, track } from '@vercel/analytics';
 import { createApp } from 'vue';
 import router from './router';
 import App from './App.vue';
 import './style.css';
 
 inject();
-router.afterEach(() => {
-    if (window.va) {
-        window.va.track('pageview')
-    }
+router.afterEach((to) => {
+    track('pageview', { url: to.fullPath });
 });
 
 createApp(App).use(router).mount('#app');
